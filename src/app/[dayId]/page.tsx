823d4e58c4713aa3c43ce2792f121afe6d94bd56
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-07 19:03:44 +0900

    style/#8 : text font. size

--- src/app/[dayId]/page.tsx
+++ src/app/[dayId]/page.tsx
@@ -7,7 +7,7 @@ interface pagePropTypes {
 
 const page = (props: pagePropTypes) => {
   return (
-    <div className="h-whole_height w-whole_width border border-black">
+    <div className="h-whole_height w-whole_width border border-black bg-skyBlue-gradient bg-blue-900">
       {/* <div className="box-content h-footer w-375rem border border-black d-flex justify-content-end align-items-end"></div> */}
       {props.dayId}번째날의 공연순서페이지입니다
       <Footer />
--- src/app/_commons/Footer.tsx
+++ src/app/_commons/Footer.tsx
@@ -9,22 +9,22 @@ import PubIcon from "../_assets/PubIcon.svg";
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 h-footer w-full border border-black flex justify-center gap-10">
+    <div className="fixed bottom-0 h-footer w-full border border-black flex justify-center gap-10 ">
       <div className="flex flex-col items-center justify-center m-0 gap-2">
         <Image src={PubIcon} alt="펍아이콘" />
-        <span>공연</span>
+        <span className="text-[#CCDEF9] text-lg">공연</span>
       </div>
       <div className="flex flex-col items-center justify-center m-0 gap-2">
         <Image src={BoothIc} alt="부스아이콘" />
-        <span>부스</span>
+        <span className="text-[#CCDEF9] text-lg">부스</span>
       </div>
       <div className="flex flex-col items-center justify-center m-0 gap-2">
         <Image src={ReservationIc} alt="예약아이콘" />
-        <span>주점</span>
+        <span className="text-[#CCDEF9] text-lg">주점</span>
       </div>
       <div className="flex flex-col items-center justify-center m-0 gap-2">
         <Image src={ShowIc} alt="MY예약아이콘" />
-        <span>MY 예약</span>
+        <span className="text-[#CCDEF9] text-lg">MY 예약</span>
       </div>
     </div>
   );
