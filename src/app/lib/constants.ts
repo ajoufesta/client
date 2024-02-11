f5e4b4147bb3c6ae9a2a9a054823d15d463ceac9
jjunohj <jjunohj@gmail.com>
2024-02-11 23:37:09 +0900

    feat/#11 : 상수 파일 추가

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,34 @@
+const FESTIVAL_DATE = ["2024-02-10", "2024-02-11", "2024-02-12"];
+
+const SECTION_LIST = [
+  {
+    section: "S-1",
+    name: "성호관 앞",
+  },
+  {
+    section: "S-2",
+    name: "성호관 테라스 옆",
+  },
+  {
+    section: "N-1",
+    name: "노천극장",
+  },
+  {
+    section: "D-1",
+    name: "도서관",
+  },
+  {
+    section: "D-2",
+    name: "도서관 주차장",
+  },
+  {
+    section: "G-1",
+    name: "가온마당 1구역",
+  },
+  {
+    section: "G-2",
+    name: "가온마당 2구역",
+  },
+];
+
+export { FESTIVAL_DATE, SECTION_LIST };
