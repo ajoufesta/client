f88417a7eb4064e98f1e4389653dabb918511fa5
jjunohj <jjunohj@gmail.com>
2024-02-13 18:43:40 +0900

    feat/#15 : 프로젝트 배경 임시 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
 import React from "react";
 
 const page = () => {
-  return <div>메인페이지!</div>;
+  return (
+    <main className="flex max-w-screen-sm min-h-screen">
+      <div className="w-full bg-skyBlue-gradient bg-blue-800"></div>
+    </main>
+  );
 };
 
 export default page;
