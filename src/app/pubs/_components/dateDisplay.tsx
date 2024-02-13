ab53de2eabe911c54d568bfc3f782a2784a4c169
jjunohj <jjunohj@gmail.com>
2024-02-13 18:38:06 +0900

    feat/#15 : DateDisplay 컴포넌트 추가

---
+++ src/app/pubs/_components/dateDisplay.tsx
@@ -0,0 +1,23 @@
+import { FESTIVAL_DATE } from "@/app/lib/constants";
+import { isToday, getFormattedDate } from "@/app/lib/utils";
+import React from "react";
+
+interface DateDisplayProps {
+  selectedDay: number;
+}
+
+const DateDisplay: React.FC<DateDisplayProps> = ({ selectedDay }) => {
+  if (selectedDay === -1 || selectedDay > FESTIVAL_DATE.length) {
+    return null;
+  }
+
+  return (
+    <div className="w-auto p-3 flex justify-center items-center h-5 flex-shrink-0 rounded-[3rem] bg-blue-700 text-blue-300 text-center text-sm font-semibold">
+      {isToday(selectedDay)
+        ? "TODAY"
+        : getFormattedDate(FESTIVAL_DATE[selectedDay - 1])}
+    </div>
+  );
+};
+
+export default DateDisplay;
