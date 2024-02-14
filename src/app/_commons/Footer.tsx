55508d167464541069d1c4a1e25017168bee0a81
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-14 21:02:15 +0900

    style/#8 : hr 추가

--- src/app/_commons/Footer.tsx
+++ src/app/_commons/Footer.tsx
@@ -5,6 +5,8 @@ import BoothIc from "../../../public/boothIcon.svg";
 import PubIc from "../../../public/pubIcon.svg";
 import ReservationIc from "../../../public/reservationIcon.svg";
 import ShowIc from "../../../public/showIcon.svg";
+import FooterHr from "../../../public/footerHr.svg";
+import IconHr from "../../../public/iconHr.svg";
 import Link from "next/link";
 
 const Footer = () => {
@@ -15,45 +17,54 @@ const Footer = () => {
   };
 
   return (
-    <div className="fixed bottom-0 h-footer w-full flex justify-center gap-10 ">
-      <Link
-        href={`http://localhost:3000/1`}
-        className={`flex flex-col items-center justify-center m-0 gap-2 ${
-          selectedButton === "공연" ? "" : "opacity-50"
-        }`}
-        onClick={() => handleClick("공연")}
-      >
-        <BoothIc />
-        <span className="text-[#CCDEF9] text-lg">공연</span>
-      </Link>
-      <Link
-        href={`http://localhost:3000/booths/1/A`}
-        className={`flex flex-col items-center justify-center m-0 gap-2 ${
-          selectedButton === "부스" ? "" : "opacity-50"
-        }`}
-        onClick={() => handleClick("부스")}
-      >
-        <PubIc />
-        <span className="text-[#CCDEF9] text-lg">부스</span>
-      </Link>
-      <Link
-        href={`http://localhost:3000/pubs/1`}
-        className={`flex flex-col items-center justify-center m-0 gap-2 ${
-          selectedButton === "주점" ? "" : "opacity-50"
-        }`}
-        onClick={() => handleClick("주점")}
-      >
-        <ReservationIc />
-        <span className="text-[#CCDEF9] text-lg">주점</span>
-      </Link>
-      <div
-        className={`flex flex-col items-center justify-center m-0 gap-2 ${
-          selectedButton === "MY 예약" ? "" : "opacity-50"
-        }`}
-        onClick={() => handleClick("MY 예약")}
-      >
-        <ShowIc />
-        <span className="text-[#CCDEF9] text-lg">MY 예약</span>
+    <div className="fixed bottom-0">
+      <FooterHr className="relative" />
+      <div className="h-footer w-full flex justify-center gap-10">
+        <Link
+          href={`http://localhost:3000/1`}
+          className={`flex flex-col items-center justify-center m-0 gap-2 ${
+            selectedButton === "공연" ? "" : "opacity-50"
+          }`}
+          onClick={() => handleClick("공연")}
+        >
+          {selectedButton === "공연" && <IconHr className="absolute top-0" />}
+          <BoothIc />
+          <span className="text-[#CCDEF9] text-lg">공연</span>
+        </Link>
+        <Link
+          href={`http://localhost:3000/booths/1/A`}
+          className={`flex flex-col items-center justify-center m-0 gap-2 ${
+            selectedButton === "부스" ? "" : "opacity-50"
+          }`}
+          onClick={() => handleClick("부스")}
+        >
+          {selectedButton === "부스" && <IconHr className="absolute top-0" />}
+          <PubIc />
+          <span className="text-[#CCDEF9] text-lg">부스</span>
+        </Link>
+        <Link
+          href={`http://localhost:3000/pubs/1`}
+          className={`flex flex-col items-center justify-center m-0 gap-2 ${
+            selectedButton === "주점" ? "" : "opacity-50"
+          }`}
+          onClick={() => handleClick("주점")}
+        >
+          {selectedButton === "주점" && <IconHr className="absolute top-0" />}
+          <ReservationIc />
+          <span className="text-[#CCDEF9] text-lg">주점</span>
+        </Link>
+        <div
+          className={`flex flex-col items-center justify-center m-0 gap-2 ${
+            selectedButton === "MY 예약" ? "" : "opacity-50"
+          }`}
+          onClick={() => handleClick("MY 예약")}
+        >
+          {selectedButton === "MY 예약" && (
+            <IconHr className="absolute top-0" />
+          )}
+          <ShowIc />
+          <span className="text-[#CCDEF9] text-lg">MY 예약</span>
+        </div>
       </div>
     </div>
   );
