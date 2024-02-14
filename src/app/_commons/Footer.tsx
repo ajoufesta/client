47db30573635c5cc60e42ac2dd3c549f064efe35
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-14 18:42:42 +0900

    feat/#8 : 클릭시 opacity50% 적용

--- src/app/_commons/Footer.tsx
+++ src/app/_commons/Footer.tsx
@@ -1,28 +1,54 @@
-import React from "react";
+"use client";
 
-import Image from "next/image";
-
-import ReservationIc from "../_assets/ReservationIcon.svg";
-import ShowIc from "../_assets/showicon.svg";
-import PubIcon from "../_assets/PubIcon.svg";
+import React, { useState } from "react";
+import BoothIc from "../../../public/boothIcon.svg";
+import PubIc from "../../../public/pubIcon.svg";
+import ReservationIc from "../../../public/reservationIcon.svg";
+import ShowIc from "../../../public/showIcon.svg";
 
 const Footer = () => {
+  const [selectedButton, setSelectedButton] = useState("");
+
+  const handleClick = (buttonName: string) => {
+    setSelectedButton(buttonName === selectedButton ? "" : buttonName);
+  };
+
   return (
     <div className="fixed bottom-0 h-footer w-full border border-black flex justify-center gap-10 ">
-      <div className="flex flex-col items-center justify-center m-0 gap-2">
-        <Image src="/boothIcon.svg" alt="펍아이콘" width={60} height={60} />
+      <div
+        className={`flex flex-col items-center justify-center m-0 gap-2 ${
+          selectedButton === "공연" ? "" : "opacity-50"
+        }`}
+        onClick={() => handleClick("공연")}
+      >
+        <BoothIc />
         <span className="text-[#CCDEF9] text-lg">공연</span>
       </div>
-      <div className="flex flex-col items-center justify-center m-0 gap-2">
-        <Image src={PubIcon} alt="부스아이콘" />
+      <div
+        className={`flex flex-col items-center justify-center m-0 gap-2 ${
+          selectedButton === "부스" ? "" : "opacity-50"
+        }`}
+        onClick={() => handleClick("부스")}
+      >
+        <PubIc />
         <span className="text-[#CCDEF9] text-lg">부스</span>
       </div>
-      <div className="flex flex-col items-center justify-center m-0 gap-2">
-        <Image src={ReservationIc} alt="예약아이콘" />
+      <div
+        className={`flex flex-col items-center justify-center m-0 gap-2 ${
+          selectedButton === "주점" ? "" : "opacity-50"
+        }`}
+        onClick={() => handleClick("주점")}
+      >
+        <ReservationIc />
         <span className="text-[#CCDEF9] text-lg">주점</span>
       </div>
-      <div className="flex flex-col items-center justify-center m-0 gap-2">
-        <Image src={ShowIc} alt="MY예약아이콘" />
+      <div
+        className={`flex flex-col items-center justify-center m-0 gap-2 ${
+          selectedButton === "MY 예약" ? "" : "opacity-50"
+        }`}
+        onClick={() => handleClick("MY 예약")}
+      >
+        <ShowIc />
         <span className="text-[#CCDEF9] text-lg">MY 예약</span>
       </div>
     </div>
