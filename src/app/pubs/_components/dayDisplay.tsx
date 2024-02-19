dcd4a76322e33946895bb6ab5a84ce768eaa2d45
jjunohj <jjunohj@gmail.com>
2024-02-19 14:58:14 +0900

    refactor/#21 : queryUrl을 DayDisplay를 조작할 때마다 생성하는 이슈 해결

--- src/app/pubs/_components/dayDisplay.tsx
+++ src/app/pubs/_components/dayDisplay.tsx
@@ -4,6 +4,7 @@ import Link from "next/link";
 import { FESTIVAL_DATE } from "@/app/lib/constants";
 import { useQueryWithDefault } from "@/app/hooks/useQueryWithDefault";
 import { useToggle } from "@/app/hooks/useToggle";
+import { useMemo } from "react";
 
 const DayDisplay = ({ selectedDay }: { selectedDay: number }) => {
   const { getQueryParam, getQueryUrl } = useQueryWithDefault();
@@ -11,9 +12,17 @@ const DayDisplay = ({ selectedDay }: { selectedDay: number }) => {
 
   const selectedSection = getQueryParam("section", "seongho1");
 
+  const queryUrls = useMemo(
+    () =>
+      FESTIVAL_DATE.map((date, index) =>
+        getQueryUrl(index + 1, selectedSection),
+      ),
+    [selectedSection],
+  );
+
   return (
     <div
-      className="flex flex-col items-center w-auto font-medium relative cursor-pointer"
+      className="flex z-20 flex-col items-center w-auto font-medium relative cursor-pointer"
       onClick={handleToggle}
     >
       <div className="pl-8 text-white text-center text-2xl font-bold w-full p-2 flex items-center justify-center">
@@ -35,9 +44,7 @@ const DayDisplay = ({ selectedDay }: { selectedDay: number }) => {
               key={index}
               className={`px-10 py-1 text-base text-center font-normal hover:bg-blue-100 hover:text-blue-400 hover:font-semibold`}
             >
-              <Link href={getQueryUrl(index + 1, selectedSection)}>
-                DAY {index + 1}
-              </Link>
+              <Link href={queryUrls[index]}>DAY {index + 1}</Link>
             </li>
           ))}
         </ul>
