457ff5337046ac9113ad49819a332469ac80c812
jjunohj <jjunohj@gmail.com>
2024-02-19 13:24:50 +0900

    feat/#23 : 주점의 위치를 설정할 수 있는 PUB_LOCATIONS 상수 데이터 추가

--- src/app/lib/types.ts
+++ src/app/lib/types.ts
@@ -3,9 +3,24 @@ export interface Pub {
   pubName: string;
   description: string;
   pubLocation: string;
-  pubHours: [string, string]; // ["10:00", "23:00"] 형식
   teamName: string;
+  pubHours: [string, string]; // ["10:00", "23:00"] 형식
   menuImageSrc: string;
   link: string;
   linkIconId: "instagram" | "picture" | "default";
 }
+
+export type PubLocation = {
+  location: string;
+  x: number;
+  y: number;
+  rotate: number;
+};
+
+type Sections = {
+  [key: string]: PubLocation[];
+};
+
+export type PubLocations = {
+  [key: number]: Sections;
+};
