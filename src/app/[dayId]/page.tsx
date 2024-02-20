f3289ab2ae642d24be865da5bbbe927e6fa11cd4
jjunohj <jjunohj@gmail.com>
2024-02-20 19:40:36 +0900

    fix/#27 : eslint build error 수정

--- src/app/[dayId]/page.tsx
+++ src/app/[dayId]/page.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 
-interface pagePropTypes {
-  dayId: string;
-}
-
-const page = (props: pagePropTypes) => {
-  return <div>{props.dayId}번째날의 공연순서페이지입니다</div>;
+const page = () => {
+  return <div>번째날의 공연순서페이지입니다</div>;
 };
 
 export default page;
