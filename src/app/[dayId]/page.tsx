012d7da96be4c26cff82aab753958a6a9e88d5c3
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-20 15:36:29 +0900

    fix: yarn build error

--- src/app/[dayId]/page.tsx
+++ src/app/[dayId]/page.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-interface pagePropTypes {
-  dayId: string;
-}
+// interface pagePropTypes {
+//   dayId: string;
+// }
 
-const page = (props: pagePropTypes) => {
-  return <div>{props.dayId}번째날의 공연순서페이지입니다</div>;
+const page = () => {
+  return <div>dk</div>;
 };
 
 export default page;
