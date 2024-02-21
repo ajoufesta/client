f4d983ed50c64847f46b9569925982afd936954c
jjunohj <jjunohj@gmail.com>
2024-02-21 22:11:58 +0900

    refactor/#34 : 주점 페이지 내 모든 컴포넌트 공통 컴포넌트로 변경

--- src/app/_commons/placeNavigator/placeCard.tsx
+++ src/app/_commons/placeNavigator/placeCard.tsx
@@ -1,43 +1,37 @@
-interface PubDataPropType {
-  pubId: null;
-  pubName: string;
-  teamName: string;
-  phoneNum: string;
-  description: string;
-  menuImageSrc: string;
-  link: string;
-  linkIconId: string;
-  pubLocation: string;
-}
+import { Place } from "@/app/lib/types";
 
-const PubCard = ({ pub }: { pub: PubDataPropType }) => {
-  if (!pub) {
-    return null;
-  }
+const PlaceCard = ({ place }: { place: Place }) => {
   return (
     <div className="w-[14.2rem] h-[11.8rem] flex-shrink-0 relative bg-white rounded-2xl">
       <div className="w-full h-full flex flex-col items-start p-[1.2rem]">
-        <span className="text-xl font-bold text-blue-400">{pub.pubName}</span>
+        <span className="text-xl font-bold text-blue-400">
+          {place && "boothId" in place ? place.boothName : place.pubName}
+        </span>
         <span className="text-normal text-blue-400 highlight">
-          {pub.teamName}
+          {place.teamName}
         </span>
         <p className="mt-1 w-[9.4rem] h-[4.8rem] flex-shrink-0 text-sm text-gray-400 overflow-hidden">
-          {pub.description}
+          {place.description}
         </p>
 
-        {/* <span className="absolute bottom-2 w-full flex-shrink-0 text-[0.8rem] text-blue-400">
-          <span className="font-semibold mr-2">운영시간</span>
-          {pub.data[0].pubHours[0]} ~ {pub.pubHours[1]}
-        </span> */}
-        <img src={pub.menuImageSrc} alt="menu" />
+        {place && "boothId" in place ? (
+          <span className="absolute bottom-2 w-full flex-shrink-0 text-[0.8rem] text-blue-400">
+            <span className="font-semibold mr-2">운영시간</span>
+            {place.openTime} ~ {place.closeTime}
+          </span>
+        ) : (
+          ""
+        )}
 
         <div className="absolute top-[1.2rem] right-[1.2rem] flex flex-col gap-1">
           <div className="w-[2.2rem] h-[2.2rem] rounded-full bg-blue-400 flex justify-center items-center">
             <span className="mt-[0.1rem] text-lg font-bold text-white">
-              {pub.pubLocation}
+              {place && "boothId" in place
+                ? place.boothLocation
+                : place.pubLocation}
             </span>
           </div>
-          <a href={pub.link} target="_blank" rel="noreferrer">
+          <a href={place.link} target="_blank" rel="noreferrer">
             <div className="w-[2.2rem] h-[2.2rem] rounded-full bg-gray-50 flex justify-center items-center"></div>
           </a>
         </div>
@@ -46,4 +40,4 @@ const PubCard = ({ pub }: { pub: PubDataPropType }) => {
   );
 };
 
-export default PubCard;
+export default PlaceCard;
