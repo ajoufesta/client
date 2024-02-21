d55ac6fed70a7eabbadcc5c9aee9f10089790750
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-22 06:25:41 +0900

    style/#36 : 세부 스타일 수정

--- src/app/stage/_components/Schedule.tsx
+++ src/app/stage/_components/Schedule.tsx
@@ -3,12 +3,27 @@ import ScheduleGradient from "../../../../public/scheduleGradient.svg";
 import ScheduleTitle from "./ScheduleTitle";
 import ScheduleTitleWrapper from "./ScheduleTitleWrapper";
 import StageCurrentButton from "./StageCurrentButton";
+import { useState, useEffect } from "react";
 
 const Schedule = () => {
   return (
-    <div className="w-[27.4rem] h-[30.7rem] flex flex-col items-center relative rounded-[1rem] bg-blue-700 ">
+    <div className="w-[27.4rem] h-[30.7rem] min-h-[30.7rem] flex flex-col items-center relative rounded-[1rem] bg-blue-700">
       <ScheduleTitleWrapper />
-      <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+      <div className="min-h-[23rem] overflow-y-scroll w-[27.4rem] h-[22rem] px-[2.5rem]">
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+        <StageCurrentButton startTime="16:30" showName="리틀 장범준" />
+      </div>
+
       <ScheduleGradient className="absolute bottom-0" />
     </div>
   );
--- src/app/stage/_components/ScheduleTitleWrapper.tsx
+++ src/app/stage/_components/ScheduleTitleWrapper.tsx
@@ -3,7 +3,7 @@ import ScheduleTitle from "./ScheduleTitle";
 
 const ScheduleTitleWrapper = () => {
   return (
-    <div className="h-[5.3rem] w-full flex  justify-between px-[4.1rem] mb-3">
+    <div className="h-[5.3rem] w-full flex  justify-between px-[4.1rem] mb-4">
       <div className="flex flex-col items-center">
         <ScheduleTitle>시간</ScheduleTitle>
         <hr className="h-[0.1rem] w-[5.1rem] rounded-[1rem]"></hr>
