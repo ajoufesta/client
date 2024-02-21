511912c72ab2b478b85e6fe020082e3a91ee5079
jjunohj <jjunohj@gmail.com>
2024-02-21 22:07:51 +0900

    chore/#34 : 파일명 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Footer from "./_commons/Footer";
+import Footer from "./_commons/footer";
 
 export const metadata: Metadata = {
   title: "Ajou Festa",
