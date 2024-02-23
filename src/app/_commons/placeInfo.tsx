3ae9e8c9101478448aa1f8fac486e4d8d93b3155
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-24 03:37:03 +0900

    fix/#44 : yarn build error

--- src/app/_commons/placeInfo.tsx
+++ src/app/_commons/placeInfo.tsx
@@ -5,23 +5,25 @@ const PlaceInfo = ({ place }: { place: Place }) => {
   return (
     <div className="w-[26.1rem] h-[15.9rem] flex-shrink-0 bg-white border border-blue-400 rounded-xl p-8 relative">
       <h1 className="w-[18.4rem] block text-2xl leading-4 font-bold text-blue-400 mb-2">
-        {place && "boothId" in place ? place.boothName : place.pubName}
+        {place && "boothId" in place && place.boothName}
+        {place && "pubId" in place && place.pubName}
+        {place && "clubId" in place && place.clubName}
       </h1>
       <span className="mt-2 text-lg font-normal text-blue-400 highlight">
-        {place.teamName}
+        {/* {place.teamName} */}
       </span>
       <p className="mt-2 w-[18rem] text-lg font-light text-gray-300 overflow-auto">
-        {place.description}
+        {/* {place.description} */}
       </p>
       <div className="absolute bottom-8 w-[calc(100%-4rem)] h-5 flex justify-between items-center">
         <div className="w-[4.6rem] h-[1.6rem] flex justify-center items-center rounded-xl bg-blue-100">
-          <span className="table-cell align-middle text-center text-base font-semibold text-blue-400 ">
+          {/* <span className="table-cell align-middle text-center text-base font-semibold text-blue-400 ">
             {place && "boothId" in place
               ? "운영시간"
               : place.isOpen
-                ? "영업중"
-                : "영업종료"}
-          </span>
+              ? "영업중"
+              : "영업종료"}
+          </span> */}
         </div>
         {place && "pubId" in place && (
           <div className="w-auto flex justify-between items-center">
@@ -41,9 +43,9 @@ const PlaceInfo = ({ place }: { place: Place }) => {
       <div className="absolute top-6 right-7 flex flex-col gap-1">
         <div className="w-[2.7rem] h-[2.7rem] rounded-full bg-blue-400 flex justify-center items-center">
           <span className="text-xl text-center font-bold text-white">
-            {place && "boothId" in place
+            {/* {place && "boothId" in place
               ? place.boothLocation
-              : place.pubLocation}
+              : place.pubLocation} */}
           </span>
         </div>
         <a href={place.link} target="_blank" rel="noreferrer">
