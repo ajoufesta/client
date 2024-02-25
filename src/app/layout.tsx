ae50ce0bf6483ff95baceeff723a4938e2bf7232
jjunohj <jjunohj@gmail.com>
2024-02-25 09:57:55 +0900

    fix/#48 : 모바일 뷰 대응 레이아웃 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import Footer from "@/app/_commons/Footer";
 
 export const metadata: Metadata = {
-  title: "Ajou Festa",
+  title: "동박 가이드 : 동화",
   description: "아주대학교 축제 도우미",
+};
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 1,
+  userScalable: false,
 };
 
 export default function RootLayout({
@@ -13,12 +21,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className="flex items-center justify-center">
-        <div className="h-screen w-screen min-h-[80rem] max-w-screen-sm  bg-blue-800 bg-skyBlue-gradient relative flex flex-col pt-[8.8rem] items-center">
-          <main className=" flex flex-col justify-center items-center">
+        <div className="h-screen w-screen max-w-screen-sm bg-blue-800 bg-skyBlue-gradient relative flex flex-col items-center">
+          <div className="h-[calc(100vh-10.4rem)] flex flex-col justify-center items-center">
             {children}
-          </main>
+          </div>
           <Footer />
         </div>
       </body>
