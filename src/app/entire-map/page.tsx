87b7fca100c9fe446eacd427ce0f9e832d2b20ba
jjunohj <jjunohj@gmail.com>
2024-02-26 16:38:55 +0900

    feat/#56 : 전체 지도 페이지 entire-map 추가

--- src/app/entire-map/page.tsx
+++ src/app/entire-map/page.tsx
@@ -1,8 +1,10 @@
+import Image from "next/image";
 import DateDisplay from "../_commons/dateDisplay";
 import DayDisplay from "../_commons/dayDisplay";
 import { getCurrentDay } from "../lib/utils";
-
+import SectionNavigator from "./_components/sectionNavigator";
 import Tutorial from "./_components/tutorial";
+import UnderBar from "./_components/underBar";
 
 interface EntireMapPageProps {
   searchParams?: {
@@ -22,8 +24,21 @@ const EntireMapPage = ({ searchParams }: EntireMapPageProps) => {
   return (
     <>
       <DateDisplay selectedDay={selectedDay} />
-      <DayDisplay selectedDay={selectedDay} />
-      <Tutorial />
+      <h1 className="text-4xl font-normal text-center mt-4 text-white mb-4">
+        동아리 박람회
+      </h1>
+      <div className="flex mt-2 w-[33.4rem] h-[49.9rem] flex-col items-center rounded-xl overflow-hidden relative">
+        <Image
+          src="/wholeMap.png"
+          alt="wholeMap"
+          width={1000}
+          height={1000}
+          priority
+        />
+        <UnderBar />
+        <SectionNavigator />
+        <Tutorial />
+      </div>
     </>
   );
 };
