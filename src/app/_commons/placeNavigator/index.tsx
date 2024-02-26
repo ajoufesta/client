1786155ab8ea06ecd964dea2fda8941cc374b759
jjunohj <jjunohj@gmail.com>
2024-02-27 02:46:04 +0900

    style/#68 : 변경된 스타일 적용

--- src/app/_commons/placeNavigator/index.tsx
+++ src/app/_commons/placeNavigator/index.tsx
@@ -1,16 +1,15 @@
 "use client";
 
-import { Place } from "@/app/lib/types";
+import { Club, Place } from "@/app/lib/types";
 import { useRef, useEffect } from "react";
 import NavigatorHandle from "@/public/navigator-handle.svg";
-import NavigatorHandleArrow from "@/public/navigator-handle-arrow-left.svg";
-import PlaceCard from "./placeCard";
 import useSelectedLocationStore from "@/app/hooks/useSelectedLocationStore";
 import { DONGBAK_LOCATIONS } from "@/app/lib/constants";
 import { getLocationByPlace } from "@/app/lib/utils";
 import useModalStore from "@/app/hooks/useModalStore";
-import PlaceInfo from "../placeInfo";
+import ClubInfo from "../clubInfo";
 import useIsOpenStore from "@/app/hooks/useIsOpenStore";
+import ClubCard from "./clubCard";
 
 interface PlaceNavigatorProps {
   places: Place[];
@@ -31,33 +30,33 @@ const PlaceNavigator = ({
   const divRef = useRef<HTMLDivElement>(null);
   let initialPosition = 0;
 
-  const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
-    initialPosition = e.touches[0].clientX;
-  };
+  // const handleTouchStart = (e: React.TouchEvent<HTMLDivElement>) => {
+  //   initialPosition = e.touches[0].clientX;
+  // };
 
-  const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
-    if (!divRef.current) return;
-    const newPosition = e.touches[0].clientX;
-    const diffX = initialPosition - newPosition;
+  // const handleTouchMove = (e: React.TouchEvent<HTMLDivElement>) => {
+  //   if (!divRef.current) return;
+  //   const newPosition = e.touches[0].clientX;
+  //   const diffX = initialPosition - newPosition;
 
-    if (initialPosition > window.innerWidth / 2 && diffX > 0) {
-      divRef.current.style.transform = `translateX(calc(${-diffX}px - 79%))`;
-    } else if (initialPosition < window.innerWidth / 2 && diffX < 0) {
-      divRef.current.style.transform = `translateX(calc(${-diffX}px - 169%)`;
-    }
-  };
+  //   if (initialPosition > window.innerWidth / 2 && diffX > 0) {
+  //     divRef.current.style.transform = `translateX(calc(${-diffX}px - 77%))`;
+  //   } else if (initialPosition < window.innerWidth / 2 && diffX < 0) {
+  //     divRef.current.style.transform = `translateX(calc(${-diffX}px - 169%)`;
+  //   }
+  // };
 
-  const handleTouchEnd = () => {
-    if (!divRef.current) return;
+  // const handleTouchEnd = () => {
+  //   if (!divRef.current) return;
 
-    if (divRef.current.getBoundingClientRect().left < window.innerWidth / 2) {
-      setIsNavOpen(true);
-      divRef.current.style.transform = "translateX(-169%)";
-    } else {
-      setIsNavOpen(false);
-      divRef.current.style.transform = "translateX(-79%)";
-    }
-  };
+  //   if (divRef.current.getBoundingClientRect().left < window.innerWidth / 2) {
+  //     setIsNavOpen(true);
+  //     divRef.current.style.transform = "translateX(-169%)";
+  //   } else {
+  //     setIsNavOpen(false);
+  //     divRef.current.style.transform = "translateX(-77%)";
+  //   }
+  // };
 
   const handleClickHandle = (e: React.MouseEvent<HTMLDivElement>) => {
     if (!isNavOpen) {
@@ -66,13 +65,13 @@ const PlaceNavigator = ({
       setIsNavOpen(true);
       if (divRef.current) {
         divRef.current.style.transition = "transform 0.2s ease-out";
-        divRef.current.style.transform = "translateX(-169%)";
+        divRef.current.style.transform = "translateX(-166%)";
       }
     } else {
       setIsNavOpen(false);
       if (divRef.current) {
         divRef.current.style.transition = "transform 0.2s ease-out";
-        divRef.current.style.transform = "translateX(-79%)";
+        divRef.current.style.transform = "translateX(-77%)";
       }
     }
   };
@@ -81,7 +80,7 @@ const PlaceNavigator = ({
     if (!isNavOpen) {
       if (divRef.current) {
         divRef.current.style.transition = "transform 0.2s ease-out";
-        divRef.current.style.transform = "translateX(-79%)";
+        divRef.current.style.transform = "translateX(-77%)";
       }
     }
   }, [isNavOpen]);
@@ -89,24 +88,19 @@ const PlaceNavigator = ({
   return (
     <div className={`z-20 absolute w-[19.7rem] h-full -right-full`}>
       <div
-        className="relative flex justify-end w-full h-full transform translate-x-[-79%]"
+        className="relative flex justify-end w-full h-full transform translate-x-[-77%]"
         ref={divRef}
       >
         <div
-          onTouchStart={handleTouchStart}
-          onTouchMove={handleTouchMove}
-          onTouchEnd={handleTouchEnd}
+          // onTouchStart={handleTouchStart}
+          // onTouchMove={handleTouchMove}
+          // onTouchEnd={handleTouchEnd}
           onClick={handleClickHandle}
         >
           <NavigatorHandle className="mt-[1.6rem]" />
-          <div className="absolute top-[2.9rem] left-[0.8rem]">
-            <NavigatorHandleArrow
-              className={`transform ${isNavOpen ? "rotate-0" : "rotate-180"}`}
-            />
-          </div>
         </div>
         <div
-          className={`flex flex-col p-4 gap-4 items-center w-full bg-transparentBlue-300 overflow-y-auto ${
+          className={`flex flex-col p-4 gap-4 items-center w-full bg-brown-500 overflow-y-auto ${
             isNavOpen ? "z-20" : "touch-none pointer-events-none"
           }`}
         >
@@ -122,16 +116,16 @@ const PlaceNavigator = ({
                     DONGBAK_LOCATIONS[selectedDay][selectedSection],
                   ),
                 );
-                setModalContent(<PlaceInfo place={place} />);
+                setModalContent(<ClubInfo place={place as Club} />);
                 openModal();
                 setIsNavOpen(false);
                 if (divRef.current) {
                   divRef.current.style.transition = "transform 0.2s ease-out";
-                  divRef.current.style.transform = "translateX(-79%)";
+                  divRef.current.style.transform = "translateX(-77%)";
                 }
               }}
             >
-              <PlaceCard place={place} />
+              <ClubCard place={place as Club} />
             </div>
           ))}
         </div>
