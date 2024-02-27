04a9237db78653e85671a2dcddbfdcee0182f1e1
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-27 17:29:17 +0900

    style: 캐러셀 주변 스타일링

--- src/app/dongwha/_components/dongyeonCarousel.tsx
+++ src/app/dongwha/_components/dongyeonCarousel.tsx
@@ -36,13 +36,17 @@ const DongyeonCarousel: React.FC<PropType> = (props) => {
       <div className="embla__viewport dongyeon" ref={emblaRef}>
         <div className="embla__container dongyeon">
           {slides.map((index) => (
-            <div className="embla__slide dongyeon" key={index}>
-              <div className="embla__slide__number dongyeon">{index + 1}</div>
+            <div className="relative embla__slide dongyeon" key={index}>
+              <div className="absolute right-[1.5rem] top-[1.7rem] w-[7.3rem] h-[3.4rem] rounded-[5rem] bg-brown-500 text-center flex justify-center items-center text-[2rem] text-white">
+                {index + 1} / 8
+              </div>
+
               <Image
                 src={`/dongbak${index + 1}.jpeg`}
                 alt="df"
                 width={335}
                 height={335}
+                className="donyeon_image"
               />
             </div>
           ))}
--- src/app/dongwha/page.tsx
+++ src/app/dongwha/page.tsx
@@ -13,10 +13,11 @@ const page = () => {
     <div>
       {" "}
       <div className="flex flex-col justify-center items-center h-[33.5rem]">
-        <main className="bg-white w-[33.5rem] h-[33.5rem] rounded-[1.5rem]">
+        <main className="relative bg-white w-[33.5rem] h-[33.5rem] rounded-[1.5rem] flex justify-center">
           <section className="sandbox__carousel">
             <DongyeonCarousel slides={SLIDES} options={OPTIONS} />
           </section>
+          <hr className="absolute bottom-0 h-[0.4rem] w-[15.9rem] bg-brown-400 rounded-[1rem]" />
         </main>
       </div>
     </div>
