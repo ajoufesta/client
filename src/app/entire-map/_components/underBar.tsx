27ff69b2db5fb5247d6e66ed63856eacd491e1d5
jjunohj <jjunohj@gmail.com>
2024-02-28 16:05:49 +0900

    style/#81 : 불필요한 div 제거

--- src/app/entire-map/_components/underBar.tsx
+++ src/app/entire-map/_components/underBar.tsx
@@ -11,23 +11,21 @@ const UnderBar = () => {
 
   return (
     <>
-      <div className="absolute w-full h-full">
-        <div className="absolute bottom-0 w-full h-[5rem] flex flex-row justify-between items-center bg-transparentWhite-100 px-[2.4rem] py-4 z-10 border-brown-500">
-          <EntireMap />
-          <span className="w-[13.8rem] font-semibold text-3xl text-center text-brown-500">
-            전체지도
-          </span>
-          <div>
-            <button
-              onClick={router.back}
-              className={`flex flex-col items-center justify-center ${
-                cameFromSection === true ? "visible" : "invisible"
-              }`}
-            >
-              <Exit />
-              <span className="text-brown-500">돌아가기</span>
-            </button>
-          </div>
+      <div className="absolute bottom-0 w-full h-[5rem] flex flex-row justify-between items-center bg-transparentWhite-200 border-t-2 px-[2.4rem] py-4 z-10 border-brown-500 backdrop-blur-sm">
+        <EntireMap />
+        <span className="w-[13.8rem] font-semibold text-3xl text-center text-brown-500">
+          전체지도
+        </span>
+        <div>
+          <button
+            onClick={router.back}
+            className={`flex flex-col items-center justify-center ${
+              cameFromSection === true ? "visible" : "invisible"
+            }`}
+          >
+            <Exit />
+            <span className="text-brown-500">돌아가기</span>
+          </button>
         </div>
       </div>
     </>
