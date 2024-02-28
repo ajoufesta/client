6e65d5a5249f30b21c2408abeff17174ca7f5867
jjunohj <jjunohj@gmail.com>
2024-02-29 00:08:39 +0900

    feat/#87 : /로 접속하면 /entire-map으로 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import { redirect } from "next/navigation";
+
 // "/"로 접속했을 때 "/entire-map"으로 리다이렉트
 const page = () => {
-  return <div></div>;
+  redirect("/entire-map");
 };
 
 export default page;
