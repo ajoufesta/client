4cac7171ac90f29b962c1e3f32ae8e2075414ac7
Moon Dahyun <dahyun723@ajou.ac.kr>
2024-02-28 18:10:14 +0900

    fix/#84 : 캐러셀 안 이미지 가운데 정렬

--- src/app/dongwha/_components/dongyeonCarousel.tsx
+++ src/app/dongwha/_components/dongyeonCarousel.tsx
@@ -14,21 +14,22 @@ const DongyeonCarousel: React.FC<PropType> = (props) => {
   const [emblaRef, emblaApi] = useEmblaCarousel(options);
 
   return (
-    <section className="embla_dongyeon">
+    <section className="embla_dongyeon flex items-center mx-auto">
       <div className="embla__viewport dongyeon" ref={emblaRef}>
-        <div className="embla__container dongyeon">
+        <div className="embla__container dongyeon flex items-center mx-auto">
           {slides.map((index) => (
-            <div className="relative embla__slide dongyeon" key={index}>
+            <div
+              className="relative embla__slide dongyeon flex flex-col items-center"
+              key={index}
+            >
               <div className="absolute right-[1rem] top-[1.7rem] w-[7.3rem] h-[3.4rem] rounded-[5rem] bg-brown-500 text-center flex justify-center items-center text-[2rem] text-white">
                 {index + 1} / 8
               </div>
-
               <Image
                 src={`/dongbak${index + 1}.jpeg`}
-                alt="df"
-                width={335}
-                height={335}
-                className="donyeon_image"
+                alt="이미지"
+                width={315}
+                height={315}
               />
             </div>
           ))}
--- src/app/dongwha/page.tsx
+++ src/app/dongwha/page.tsx
@@ -14,9 +14,7 @@ const page = () => {
       <DongyeonButton />
       <div className="flex flex-col justify-center items-center h-[33.5rem]">
         <main className="relative bg-white w-[33.5rem] h-[33.5rem] rounded-[1.5rem] flex justify-center">
-          <section className="sandbox__carousel">
-            <DongyeonCarousel slides={SLIDES} options={OPTIONS} />
-          </section>
+          <DongyeonCarousel slides={SLIDES} options={OPTIONS} />
           <hr className="absolute bottom-0 h-[0.4rem] w-[15.9rem] bg-brown-400 rounded-[1rem]" />
         </main>
       </div>
