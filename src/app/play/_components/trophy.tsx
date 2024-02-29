82eb7f780e3200eaba64ec49f2f255f3ea1430ef
jjunohj <jjunohj@gmail.com>
2024-02-29 20:00:56 +0900

    design/#95 : 트로피 컴포넌트 디자인 수정

--- src/app/play/_components/trophy.tsx
+++ src/app/play/_components/trophy.tsx
@@ -1,12 +1,12 @@
 import { GamePlayer } from "@/app/lib/types";
-import FirstMedal from "@/public/medal.svg";
+import ThirdMedal from "@/public/medal.svg";
 import SecondMedal from "@/public/medal2.svg";
-import ThirdMedal from "@/public/medal3.svg";
+import FirstMedal from "@/public/medal3.svg";
 
 const colors = {
-  1: "bg-brown-500",
-  2: "bg-brown-400",
-  3: "bg-brown-300",
+  1: ["bg-brown-300", "border-brown-400"],
+  2: ["bg-brown-400", "border-brown-500"],
+  3: ["bg-brown-500", "border-brown-600"],
 };
 
 const Trophy = ({ rank, player }: { rank: 1 | 2 | 3; player: GamePlayer }) => {
@@ -30,16 +30,17 @@ const Trophy = ({ rank, player }: { rank: 1 | 2 | 3; player: GamePlayer }) => {
         </span>
       </div>
       <div
-        className={`center-content w-[10.5rem] h-[11.5rem] flex-shrink-0 ${colors[rank]} rounded-t-xl rounded-b-[8rem] relative`}
+        className={`center-content w-[10.5rem] h-[11.5rem] flex-shrink-0 ${colors[rank][0]} rounded-t-xl rounded-b-[8rem] relative overflow-hidden`}
       >
-        <span className="text-3xl font-bold text-brown-600 center-txt text-center top-24">
+        <div className={`absolute top-0 w-full border-2 ${colors[rank][1]}`} />
+        <span className="w-[9rem] text-3xl font-bold text-brown-600 center-txt text-center top-24">
           {player.name}
           <br />
           <span className="text-2xl font-normal">{player.level} 년</span>
         </span>
       </div>
       <div
-        className={`mt-2 w-[5.7rem] h-[2.8rem] ${colors[rank]} rounded-t-[5rem] rounded-b-md`}
+        className={`mt-2 w-[5.7rem] h-[2.8rem] ${colors[rank][0]} rounded-t-[5rem] rounded-b-md`}
       />
     </div>
   );
