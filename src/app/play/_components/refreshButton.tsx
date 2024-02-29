0cdb5e04d9a2b3cf27c8013d6a82633067a90ddc
jjunohj <jjunohj@gmail.com>
2024-02-29 20:04:44 +0900

    feat/#95 : 기록 갱신 버튼 추가

---
+++ src/app/play/_components/refreshButton.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import RefreshIcon from "@/public/refreshIcon.svg";
+import { useState } from "react";
+
+type Props = {
+  tag: string;
+};
+
+export default function RevalidateButton({ tag }: Props) {
+  const [isClicked, setIsClicked] = useState(false);
+  const router = useRouter();
+
+  const handleClick = async () => {
+    await fetch(`/api/revalidate?tag=${tag}`, {
+      method: "POST",
+    });
+
+    setIsClicked(true);
+    setTimeout(() => {
+      setIsClicked(false);
+      router.refresh();
+    }, 1000);
+  };
+
+  return (
+    <button
+      className={`${isClicked ? "animate-loading" : ""}`}
+      onClick={handleClick}
+    >
+      <RefreshIcon />
+    </button>
+  );
+}
